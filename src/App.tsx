2bb94a456f2d630d3fab10437dcb56e9196da22b
ApaAlfa09 <josue.apariciocr@gmail.com>
2026-03-11 16:19:34 -0600

    new list items

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ function App() {
   return (
     <>
       <div>hola</div>
+
+      <div>
+        <ul>
+          <li className="item-lista">Lorem ipsum dolor sit amet.</li>
+          <li className="item-lista">Lorem ipsum dolor sit amet.</li>
+          <li className="item-lista">Lorem ipsum dolor sit amet.</li>
+        </ul>
+      </div>
     </>
   );
 }
